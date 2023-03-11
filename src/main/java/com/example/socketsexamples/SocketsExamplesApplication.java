66e78311b734f0b936059c8a5d7efd23ada5fefb
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-03-11 14:06:35 -0500

    Primer commit

---
+++ src/main/java/com/example/socketsexamples/SocketsExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketsexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketsExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketsExamplesApplication.class, args);
+    }
+
+}
